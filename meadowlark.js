1dee4193eede5752ec9ba3dbb1a38e81406b3211
Kent Richmond <Krichmond4254@conestogac.on.ca>
2017-03-22 11:26:55 -0400

    added import line and function call from fortune.js

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 
 var app = express();
@@ -12,21 +13,22 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
+/*var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
 	"Rivers need springs.",
 	"Do not fear what you don't know.",
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
-];
+];*/
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var randomFortune =
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+//	var randomFortune =
+//		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+//	res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
